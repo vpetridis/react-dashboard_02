ac171bcc62baee7bb0d68b3d97b8442b4288f21d
Vasiles Petrides <vasileios.petridis@gmail.com>
2021-01-30 14:42:26 +0200

    fixed dragging items

--- src/components/Services/ServicesDnD.js
+++ src/components/Services/ServicesDnD.js
@@ -1,35 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Services.module.css'
 
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
 
 export default function ServicesDnD(props) {
-  const children = props.children
-  const childrenArray = React.Children.map(children, (child) => child)
-  console.log(childrenArray)
+  let children = props.children
+  let childrenArray = React.Children.map(children, (child) => child)
+  const [items, setItems] = useState(childrenArray)
+
+  const reorder = (list, startIndex, endIndex) => {
+    const result = list
+    const [removed] = result.splice(startIndex, 1)
+    result.splice(endIndex, 0, removed)
+    return result
+  }
+
+  function onDragEnd(result) {
+    if (!result.destination) {
+      return
+    }
+    console.log(result.destination.index)
+
+    const items = reorder(childrenArray, result.source.index, result.destination.index)
+    setItems(items)
+  }
+
   return (
     <div className={styles.container}>
       <h3 className={styles.title}>Επιλογή Υπηρεσιών</h3>
-      <DragDropContext>
-        <Droppable droppableId="id">
-          {(provided) => (
-            <ul {...provided.droppableProps} ref={provided.innerRef}>
-              {childrenArray.map((component, index) => {
-                return (
-                  <Draggable key={component.key} draggableId={component.key} index={index}>
-                    {(provided) => (
-                      <li
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}>
-                        <div>{component}</div>
-                      </li>
-                    )}
-                  </Draggable>
-                )
-              })}
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="droppable" direction="vertical">
+          {(provided, snapshot) => (
+            <div ref={provided.innerRef} {...provided.droppableProps}>
+              {items.map((item, index) => (
+                <Draggable key={item.key} draggableId={item.key} index={index}>
+                  {(provided, snapshot) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}>
+                      {item}
+                    </div>
+                  )}
+                </Draggable>
+              ))}
               {provided.placeholder}
-            </ul>
+            </div>
           )}
         </Droppable>
       </DragDropContext>
